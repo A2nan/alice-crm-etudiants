040678f44bddb6cb6f3998d6bad2521354bef157
Mahboubi Adnan <147587966+A2nan@users.noreply.github.com>
2026-09-14 11:31:58 +0200

    Ajout de la gestion des autorisations pour les documents et création d'un fichier loader pour le traitement des modèles

--- src/Controller/DocumentController.php
+++ src/Controller/DocumentController.php
@@ -14,12 +14,15 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\String\Slugger\SluggerInterface;
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 
 
 #[Route('/document')]
+// defence in depth : access_control already covers ^/document, this survives a route prefix change
+#[IsGranted('ROLE_USER')]
 class DocumentController extends AbstractController
 {
     private $entityManager;
